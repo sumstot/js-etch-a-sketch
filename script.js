9ae55b48df6f8ceff6ca156f6ab0db44024eaba7
sumstot <soren.umstot@gmail.com>
2022-09-08 17:57:04 +0900

    I have created a barebones skeleton in html for my etch-a-sketch and have created a grid in DOM. However the grid is spaced out. Will bring the grid together in coming commits.

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const grid = document.getElementById("board-container");
+
+const createGrid = (rows, cols) => {
+  grid.style.setProperty("--grid-rows", rows);
+  grid.style.setProperty("--grid-cols", cols);
+  for (let i = 0; i < rows * cols + 1; i++) {
+    let div = document.createElement("div");
+    div.classList.add("board");
+    div.textContent = i;
+    grid.appendChild(div);
+  }
+};
+
+createGrid(16, 16);
