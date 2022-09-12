89552b23717fcfa21213d326a38dc811ad10174a
sumstot <soren.umstot@gmail.com>
2022-09-12 10:44:51 +0900

    added functionality to dynamically change the grid size. Reset function is also written. Will add change color buttons and reset buttons in next update

--- script.js
+++ script.js
@@ -3,18 +3,7 @@ const RGBMAX = 256;
 const BOARDWIDTH = 500;
 const BOARDHEIGHT = 500;
 const grid = document.getElementById("board-container");
-const black = function (e) {
-  e.target.style.backgroundColor = "black";
-};
-// TODO create a function for rainbow
-const rainbow = function (e) {
-  const rgb = [];
-  for (let i = 0; i < 3; i++) {
-    let color = Math.floor(Math.random() * RGBMAX) + 1;
-    rgb.push(color);
-  }
-  e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-};
+const slider = document.getElementById("myRange");
 
 const createGrid = (rows, cols) => {
   grid.style.setProperty("--grid-rows", rows);
@@ -31,4 +20,36 @@ const createGrid = (rows, cols) => {
   }
 };
 
-createGrid(16, 16);
+const clearGrid = () => {
+  let grid = document.querySelector("#board-container");
+  let child = grid.lastElementChild;
+  while (child) {
+    grid.removeChild(child);
+    child = grid.lastElementChild;
+  }
+};
+
+slider.oninput = function () {
+  let newValue = Math.round(
+    ((this.value - this.min) / (this.max - this.min)) * 100
+  );
+  if (newValue === 0) {
+    newValue + 1;
+  }
+  slider.setAttribute("value", newValue);
+  console.log(slider.value);
+  clearGrid();
+  createGrid(slider.value, slider.value);
+};
+
+// TODO create a function for rainbow
+const rainbow = function (e) {
+  const rgb = [];
+  for (let i = 0; i < 3; i++) {
+    let color = Math.floor(Math.random() * RGBMAX) + 1;
+    rgb.push(color);
+  }
+  e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+};
+
+createGrid(slider.value, slider.value);
